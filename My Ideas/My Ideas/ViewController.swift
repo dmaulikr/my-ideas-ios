d88ced836cfd6f2a2a8d3873476a2c0050b7da35
Julia Schwarz <julenka@cs.cmu.edu>
2015-11-28 00:43:47 -0800

    show ideas from dummy client

--- My Ideas/My Ideas/ViewController.swift
+++ My Ideas/My Ideas/ViewController.swift
@@ -17,12 +17,16 @@ class ViewController: UIViewController, UITableViewDataSource {
     var ideasClient = DummyIdeasClient()
     var ideaList = IdeaList()
     
+    let TABLEVIEW_CELL_IDENTIFIER = "reuseIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         ideasClient.connect(onConnectError, successCallback: onConnectSuccess)
         ideaList = ideasClient.getIdeaList()
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: TABLEVIEW_CELL_IDENTIFIER)
     }
     
     func onConnectError(error: NSError) {
@@ -54,16 +58,40 @@ class ViewController: UIViewController, UITableViewDataSource {
         return ideaList.getCount()
     }
     
-
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) 
-        
-        // Configure the cell...
-        let idea = ideaList.ideaAtIndex(indexPath.row)
+    func textForIdeaAtIndex(index: Int) -> String {
+        let idea = ideaList.ideaAtIndex(index)
         let formatter = NSDateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-        cell.textLabel?.text = formatter.stringFromDate(idea.dateTime) + ": " + idea.text
+        return formatter.stringFromDate(idea.dateTime) + ": " + idea.text
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(TABLEVIEW_CELL_IDENTIFIER, forIndexPath: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = textForIdeaAtIndex(indexPath.row)
+        cell.textLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+        cell.textLabel?.numberOfLines = 0;
+        cell.textLabel?.font = UIFont(name: "Helvetica", size: 14.0)
+        if (indexPath.row % 2 == 0) {
+            cell.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
+        } else {
+            cell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
         return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let cellText = textForIdeaAtIndex(indexPath.row)
+        let font = UIFont(name: "Helvetica", size: 17.0)
+        let constraintSize = CGSizeMake(tableView.bounds.width, CGFloat.max);
+        let attributedText = NSAttributedString(string: cellText, attributes: [
+            NSFontAttributeName: font!
+            ])
+        let labelSize = attributedText.boundingRectWithSize(constraintSize, options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil)
+
+        
+        return labelSize.height + 100;
     }
     
 }
